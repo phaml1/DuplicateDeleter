9b74f10c9d33971321440dcfe1b486f9db517632
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-12-01 23:33:09 -0500

    working on remove dup

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,37 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int count = 0;
+        for (Integer i : this.array){
+            for (Integer integer: this.array){
+                if(i.equals(integer)){
+                    count++;
+                }
+            }
+        }
+        Integer[] temp = new Integer[array.length - count];
+        int j = 0;
+        if(count == maxNumberOfDuplications){
+            for (Integer i : this.array){
+                for (Integer integer: this.array){
+                    if(!i.equals(integer)){
+                        temp[j] = i;
+                        j++;
+                    }
+                }
+            }
+        }
+        return temp;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return new Integer[0];
+    }
 }
